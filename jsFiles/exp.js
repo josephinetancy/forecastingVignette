6a1d915f09f8376f4680fd33aa523c04d9b352eb
David E Melnikoff <davidemelnikoff@gmail.com>
2025-06-06 13:47:23 -0700

    Update exp.js

--- jsFiles/exp.js
+++ jsFiles/exp.js
@@ -195,7 +195,7 @@ var textNew = {
             `<div class='parentGoal'>
             <p>Your goal in Feel the Spin is to spin the wheels and earn as many points as possible.</p>
             <p>You will see a variety of wheels, each with its own set of values and probability of a standard outcome. For each wheel, your job is to spin the wheel and earn as many points as possible.</p>
-            <p>Also, after each wheel, you will describe your feelings. Specifically, you will report how <b>immersed</b> and <b>absorbed</b> you felt spinning each wheel.</p>
+            <p>Also, after each wheel, you will describe your feelings. Specifically, you will report how <b>immersed</b> and <b>absorbed</b> you felt while spinning each wheel.</p>
             <p>Continue to the next screen to begin.</p>
             </div>`,      
         ],
